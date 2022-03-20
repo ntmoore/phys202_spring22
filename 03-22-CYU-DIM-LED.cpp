3940a5e77295bbc393336399d725648ad679e31d
Nathan Moore <ntmoore@gmail.com>
2022-03-20 09:31:21 -0500

    Create 03-22-CYU-DIM-LED.cpp

---
+++ 03-22-CYU-DIM-LED.cpp
@@ -0,0 +1,19 @@
+/*
+Dim-us
+Modified version of the blink program that delays for us instead of ms
+details on the new function here:
+  https://www.arduino.cc/reference/en/language/functions/time/delaymicroseconds/
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(10, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delayMicroseconds(200);                       // wait for a second
+  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
+  delayMicroseconds(500);                       // wait for a second
+}
