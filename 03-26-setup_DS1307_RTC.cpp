19d5606b34db1e2762cb27b0c670388e35597350
Nathan Moore <ntmoore@gmail.com>
2022-03-26 09:36:13 -0500

    Create 03-26-setup_DS1307_RTC.cpp

---
+++ 03-26-setup_DS1307_RTC.cpp
@@ -0,0 +1,65 @@
+// building of the RTClib 1307 example
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+void setup () {
+  Serial.begin(57600);
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    //
+    //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    //
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  //
+  //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(",");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.println();
+  delay(3000);
+}
+/* output
+2022/3/26,9:35:33
+
+2022/3/26,9:35:36
+
+2022/3/26,9:35:39
+
+2022/3/26,9:35:42
+*/
